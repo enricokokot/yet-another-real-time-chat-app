6b80061f732f5b7272ce296115a78553a7be34d0
coco <kokotenrico@gmail.com>
2024-02-23 10:35:11 +0100

    set up new message warnings

--- frontend/components/UsersList.js
+++ frontend/components/UsersList.js
@@ -1,12 +1,18 @@
 import { View, Button, StyleSheet, Text } from "react-native";
 
-const UsersList = ({ users, title, friendStuff, startChat }) => {
+const UsersList = ({ users, title, friendStuff, startChat, inbox }) => {
+  console.log("UsersLIstInbox: " + Array.from(inbox));
+
   return (
     <View style={styles.container}>
       <Text style={{ fontWeight: "bold" }}>{title}</Text>
       {users.map((user) => (
         <View style={styles.paddedElement} key={user}>
           <Button title={user} onPress={() => friendStuff(user)}></Button>
+          <Text>
+            Messages in inbox:{" "}
+            {inbox.filter((msg) => msg.data.fromId.includes(user)).length}
+          </Text>
           <Button title={"chat"} onPress={() => startChat(user)}></Button>
         </View>
       ))}
