56de68fa6575ed5751890d37baad5debfc43942b
coco <kokotenrico@gmail.com>
2024-04-11 10:02:11 +0200

    fix double display of users who user already has chat with

--- frontend/components/UsersList.js
+++ frontend/components/UsersList.js
@@ -2,42 +2,50 @@ import { View, StyleSheet, Pressable } from "react-native";
 import Circle from "./Circle";
 
 const UsersList = ({
-  // currentUser,
+  currentUser,
   users,
   // friendStuff,
   startChat,
   // inbox,
   activeChat,
 }) => {
+  const usersYouAlreadyChatWith = currentUser.chats
+    .map((chat) =>
+      chat.users.map((user) => user.id).filter((id) => id !== currentUser.id)
+    )
+    .filter((chat) => chat.length === 1)
+    .map((chat) => chat[0]);
   return (
     <>
-      {users.map((user) => (
-        <View key={user.id} style={styles.container}>
-          <Circle
-            content={user.username}
-            style={
-              user.id === activeChat && {
-                borderWidth: 2,
-                borderColor: "#000",
-              }
-            }
-          />
-          <Pressable
-            style={styles.bottomLeft}
-            onPress={() => startChat(user.id)}
-          >
+      {users
+        .filter((user) => !usersYouAlreadyChatWith.includes(user.id))
+        .map((user) => (
+          <View key={user.id} style={styles.container}>
             <Circle
-              style={{
-                width: 35,
-                height: 35,
-                borderWidth: 1,
-                borderColor: "#fff",
-              }}
-              content={"💬"}
+              content={user.username}
+              style={
+                user.id === activeChat && {
+                  borderWidth: 2,
+                  borderColor: "#000",
+                }
+              }
             />
-          </Pressable>
-        </View>
-      ))}
+            <Pressable
+              style={styles.bottomLeft}
+              onPress={() => startChat(user.id)}
+            >
+              <Circle
+                style={{
+                  width: 35,
+                  height: 35,
+                  borderWidth: 1,
+                  borderColor: "#fff",
+                }}
+                content={"💬"}
+              />
+            </Pressable>
+          </View>
+        ))}
     </>
   );
 };
